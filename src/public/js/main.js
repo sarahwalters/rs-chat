7a26bc5b16c1d83d4e8575e5ec1276c5af5adca9
Sarah Walters <swalters4925@gmail.com>
2016-12-25 19:31:40 -0500

    FIX: scripts -> js; ADD: simple styling

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,7 +7,9 @@
     var socket = io();
 
     // upon form submit, encode newly posted message and emit on socket to server
-    $('form').submit(function() {
+    $('#post').keypress(function(e) {
+      if (e.which != 13) return; // only submit upon enter key press
+
       var msg = $('#post').val();
       var encoded = RS.encode(msg);
       socket.emit(CONSTANTS.EVENT_TYPES.MESSAGE, encoded);
@@ -18,12 +20,14 @@
     // upon broadcasted message from server, decode message and add to screen
     socket.on(CONSTANTS.EVENT_TYPES.MESSAGE, function(encoded) {
       var decoded = RS.decode(encoded);
-      $('#messages').append($('<li>').text(decoded));
+      $('#messages').append($('<p>').text('> ' + decoded));
     });
 
     // upon update to number of connections, update connections counter
     socket.on(CONSTANTS.EVENT_TYPES.CONNECTIONS, function(connections) {
-      $('#connections').html(connections);
+      var word = connections == 1 ? 'connection' : 'connections';
+      var html = connections + ' ' + word;
+      $('#connections').html(html);
     });
   }
 
