89c1dadd481147c17785ff135a3936b6c3423c7c
ganparse <jarrett155@hotmail.com>
2017-01-13 13:24:31 -0800

    ADD: RS encoding minus generator polynomial + decoding if no errors (#1)
    * Untested shenanigans, but it compiles at least!
    On a side note, what is a good way to test in JS?
    
    * Add decode RS block function.
    
    * Add UTIL IIFE
    
    * Remove LUT check functions from being exported by UTIL IIFE.
    
    * Remove shift function from RS file and use UTIL.shift() instead.
    
    * Move util include above rs include in html file to ensure rs knows about util.
    
    * Add errors!
    
    * I found some issues with division that I fixed.
    
    * Update: fix line comment issues.

--- src/public/js/rs.js
+++ src/public/js/rs.js
@@ -5,20 +5,50 @@ var RS = (function() {
 
   function encode(msg) {
     // dummy cipher for now -- advances each letter in message by ROT
-    return shift(msg, ROT);
+    return UTIL.shift(msg, ROT);
   }
 
   function decode(msg) {
     // dummy cipher for now -- advances each letter in message by -ROT
-    return shift(msg, -ROT);
+    return UTIL.shift(msg, -ROT);
   }
 
-  function shift(msg, rot) {
-    return msg.split('').reduce(function(accumulator, letter, index) {
-      var shiftedCharCode = letter.charCodeAt() + rot;
-      var shiftedLetter = String.fromCharCode(shiftedCharCode);
-      return accumulator + shiftedLetter;
-    }, '');
+  // Encodes using systematic BCH Reed-Solomon on Galois Field 256
+  // Message should be passed in as length k block
+  // k specifies message length
+  // n specifies block length (number of symbols transmitted)
+  // n - k is the number of code symbols added.
+  // Encoding procedure has the largest order coefficient on the left
+  // and the smallest on the right. IE: [1,2,3] -> x^2 + 2x + 3
+  // The largest order coeeficient is the [0] element of msg so as
+  // to maintain the ordering of [msg, code symbols] in the coded msg.
+  function encodeRSBlock(msg, n, k) {
+    var remainder = UTIL.polynomialDiv(msg, genPoly, n, k);
+    var codedMsg = new Uint8Array(n);
+    for (var i = 0; i < k; i++) {
+      codedMsg[i] = msg[i];
+    }
+    for (i = k; i < n; i++) {
+      codedMsg[i] = remainder[i - k];
+    }
+    return codedMsg;
+  }
+
+  function decodeRSBlock(codedMsg, n, k) {
+    var remainder = UTIL.polynomialDiv(codedMsg, genPoly, n, k);
+    var errors = false;
+    for (var i = 0; i < n - k; i++) {
+      if (remainder[i] != 0) {
+        errors = true;
+        break;
+      }
+    }
+    if (errors) {
+      // TODO ERROR CORRECTION
+      throw new Error('Error correction not implemented yet');
+    } else {
+      return codedMsg.slice(0, k);
+    }
   }
 
   return {
