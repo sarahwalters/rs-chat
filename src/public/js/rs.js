750ff1d0ac91293c50903095e5470d0c70c46ca9
ganparse <Jarrett@Jarretts-MacBook-Pro.local>
2017-01-12 19:51:15 -0800

    Remove shift function from RS file and use UTIL.shift() instead.

--- src/public/js/rs.js
+++ src/public/js/rs.js
@@ -7,20 +7,12 @@ var RS = (function() {
 
   function encode(msg) {
     // dummy cipher for now -- advances each letter in message by ROT
-    return shift(msg, ROT);
+    return UTIL.shift(msg, ROT);
   }
 
   function decode(msg) {
     // dummy cipher for now -- advances each letter in message by -ROT
-    return shift(msg, -ROT);
-  }
-
-  function shift(msg, rot) {
-    return msg.split('').reduce(function(accumulator, letter, index) {
-      var shiftedCharCode = letter.charCodeAt() + rot;
-      var shiftedLetter = String.fromCharCode(shiftedCharCode);
-      return accumulator + shiftedLetter;
-    }, '');
+    return UTIL.shift(msg, -ROT);
   }
 
   // Encodes using systematic BCH Reed-Solomon on Galois Field 256
