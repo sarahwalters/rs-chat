28b70a0ecc33e47c87b3cf9d5f2bee95f82d89b5
ganparse <Jarrett@Jarretts-MacBook-Pro.local>
2017-01-24 15:33:15 -0800

    Add basic and crappy UI username functionality.

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -6,8 +6,11 @@
 
   function main(CONSTANTS) {
     var socket = io();
+    var uname = '';
     var n = 255;
     var k = 245;
+
+    $('#uname').val('delete this and enter your user name here');
 
     // upon form submit, encode newly posted message and emit on socket to server
     $('#post').keypress(function(e) {
@@ -19,30 +22,44 @@
       var msg = $('#post').val();
       var i = 1;
       var last = 0;
-      var encoded = new Array(0);
+      var encoded = {
+        data: new Array(0),
+        uname: uname
+      }
       for (; i <= msg.length; i++) {
         if (i % k == 0) {
-          Array.prototype.push.apply(encoded, RS.encode(msg.slice(i - k, i)));
+          Array.prototype.push.apply(encoded.data, RS.encode(msg.slice(i - k, i)));
           last = i;
         }
       }
-      Array.prototype.push.apply(encoded, RS.encode(msg.slice(last, i)));
+      Array.prototype.push.apply(encoded.data, RS.encode(msg.slice(last, i)));
       socket.emit(CONSTANTS.EVENT_TYPES.MESSAGE, encoded);
       $('#post').val('');
       return false;
     });
 
+    //Upon form submit set this clients username
+    $('#uname').keypress(function(e) {
+      // only submit upon enter key press
+      if (e.which != 13) {
+        return;
+      }
+      uname = $('#uname').val();
+      $('#uname').val('Your user name is: ' + uname);
+    });
+
     // upon broadcasted message from server, decode message and add to screen
     socket.on(CONSTANTS.EVENT_TYPES.MESSAGE, function(encoded) {
+      console.log(encoded)
       var decoded = new Array(0);
-      for (var i = 1; i <= encoded.length; i++) {
+      for (var i = 1; i <= encoded.data.length; i++) {
         if (i % n == 0) {
           Array.prototype.push.apply(decoded,
-              RS.decode(encoded.slice(i - n, i)));
+              RS.decode(encoded.data.slice(i - n, i)));
         }
       }
       var decodedString = decoded.join('');
-      $('#messages').append($('<p>').text('> ' + decodedString));
+      $('#messages').append($('<p>').text(encoded.uname + '> ' + decodedString));
     });
 
     // upon update to number of connections, update connections counter
