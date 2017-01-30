0d87baaa1c5843eaeedd624b232089ecc0225906
ganparse <Jarrett@Jarretts-MacBook-Pro.local>
2017-01-30 02:06:41 -0800

    Add options form and some ui "niceness" and lint fixes.

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -10,31 +10,57 @@
     var n = 255;
     var k = 245;
     $(function() {
-      $( "#login_prompt" ).dialog({
-        autoOpen: false,
+      $('#login_prompt').dialog({
+        autoOpen: true,
         buttons: {
           OK: function() {
-            $( this ).dialog( "close" );
+            $(this).dialog('close');
           }
         },
-        beforeClose: function( event, ui ) {
-          if ( $( "#login_uname" ).val() == "" ) {
+        beforeClose: function(event, ui) {
+          if ($('#login_uname').val() == '') {
             event.preventDefault();
+            $('[for = login_uname]').addClass('invalid');
           } else {
-            uname = $('#login_uname').val()
+            uname = $('#login_uname').val();
           }
         },
-        width: 600  
+        width: 600
       });
-      $( "#login_opener" ).click(function() {
-        $( "#login_prompt" ).dialog( "open" );
+      $('#login_opener').click(function() {
+        $('#login_prompt').dialog('open');
       });
     });
 
-    $('#uname').val('delete this and enter your user name here');
+    $(function() {
+      $('#parameter_prompt').dialog({
+        autoOpen: false,
+        buttons: {
+          OK: function() {
+            $(this).dialog('close');
+          }
+        },
+        beforeClose: function(event, ui) {
+          var parsedN = parseInt($('#param_n').val());
+          var parsedK = parseInt($('#param_k').val());
+          if (isNaN(parsedN) || isNaN(parsedK)) {
+            $('[for = param_n]').addClass('invalid');
+            $('[for = param_k]').addClass('invalid');
+            event.preventDefault();
+          } else {
+            n = parsedN;
+            k = parsedK;
+          }
+        }
+      });
+      $('#parameter_opener').click(function() {
+        $('#parameter_prompt').dialog('open');
+      });
+    });
 
-    // upon form submit, encode newly posted message and emit on socket to server
+    // Upon form submit, encode newly posted message and emit on socket to server.
     $('#post').keypress(function(e) {
+
       // only submit upon enter key press
       if (e.which != 13) {
         return;
@@ -48,14 +74,16 @@
         uname: uname,
         n: n,
         k: k,
-      }
+      };
       for (; i <= msg.length; i++) {
         if (i % k == 0) {
-          Array.prototype.push.apply(encoded.data, RS.encode(msg.slice(i - k, i), n, k));
+          Array.prototype.push.apply(encoded.data, RS.encode(
+            msg.slice(i - k, i), n, k));
           last = i;
         }
       }
-      Array.prototype.push.apply(encoded.data, RS.encode(msg.slice(last, i), n, k));
+      Array.prototype.push.apply(encoded.data, RS.encode(
+        msg.slice(last, i), n, k));
       socket.emit(CONSTANTS.EVENT_TYPES.MESSAGE, encoded);
       $('#post').val('');
       return false;
@@ -71,7 +99,8 @@
         }
       }
       var decodedString = decoded.join('');
-      $('#messages').append($('<p>').text(encoded.uname + '> ' + decodedString));
+      $('#messages').append($('<p>').text(
+        encoded.uname + '> ' + decodedString));
     });
 
     // upon update to number of connections, update connections counter
